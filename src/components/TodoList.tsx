33e78e71ea15935b77b7d194f4d2cf2274a109a4
heerokj <jungheerok@gmail.com>
2025-04-17 16:17:14 +0900

    feat : 투두리스트 추가 기능 + uuid 설치

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,21 @@
 "use client";
 import { Todo } from "@/services/todo";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
-import React from "react";
 
-export const getTodos = async () => {
+const getTodos = async () => {
   const result = await axios.get("http://localhost:3001/todos");
   return result.data;
 };
 
-export const deleteTodo = async (id: string) => {
+const addTodo = async (todo: Todo) => {
+  const result = await axios.post("http://localhost:3001/todos", todo);
+  return result;
+};
+
+const deleteTodo = async (id: string) => {
   const result = await axios.delete(`http://localhost:3001/todos/${id}`);
   return result;
 };
@@ -17,11 +23,20 @@ export const deleteTodo = async (id: string) => {
 //SECTION - 리스트페이지
 export default function TodoList() {
   const queryClient = useQueryClient();
+  const [title, setTitle] = useState("");
 
   //useQuery - getTodos
   const { data, isLoading, isError } = useQuery({
     queryKey: ["todos"],
     queryFn: getTodos,
+  });
+
+  //useMutation - addTodo
+  const addMutation = useMutation({
+    mutationFn: addTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
   });
 
   // useMutation - deleteTodo
@@ -39,6 +54,20 @@ export default function TodoList() {
     deleteMutation.mutate(id);
   };
 
+  const handleAddTodo = async () => {
+    if (title.trim() !== "") {
+      addMutation.mutate({
+        id: uuidv4(),
+        title: title,
+        completed: false,
+      });
+      setTitle("");
+    } else {
+      alert("타이틀을 입력해주세요.");
+      return;
+    }
+  };
+
   return (
     <>
       <section>
@@ -54,7 +83,17 @@ export default function TodoList() {
             ))}
         </ul>
       </section>
-      <form action=""></form>
+      <form onSubmit={handleAddTodo}>
+        <input
+          type="text"
+          placeholder="할 일을 입력하세요"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          value={title}
+        />
+        <button>추가하기</button>
+      </form>
     </>
   );
 }
