eff1ae43c72071d38b7f88bd784624eb267aaae1
heerokj <jungheerok@gmail.com>
2025-04-17 15:51:04 +0900

    feat : 투두리스트 삭제 기능

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,28 +1,43 @@
 "use client";
 import { Todo } from "@/services/todo";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 
 export const getTodos = async () => {
-  try {
-    const response = await fetch("http://localhost:3001/todos");
-    if (!response.ok) throw Error("에러 발생");
-    const todoData = await response.json();
-
-    return todoData;
-  } catch (error) {
-    console.error(error);
-  }
+  const result = await axios.get("http://localhost:3001/todos");
+  return result.data;
 };
 
+export const deleteTodo = async (id: string) => {
+  const result = await axios.delete(`http://localhost:3001/todos/${id}`);
+  return result;
+};
+
+//SECTION - 리스트페이지
 export default function TodoList() {
+  const queryClient = useQueryClient();
+
+  //useQuery - getTodos
   const { data, isLoading, isError } = useQuery({
     queryKey: ["todos"],
     queryFn: getTodos,
   });
 
+  // useMutation - deleteTodo
+  const deleteMutation = useMutation({
+    mutationFn: deleteTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
   if (isLoading) return <div>로딩중입니다..</div>;
   if (isError) return <div>에러 발생..</div>;
+
+  const handleDeleteTodo = (id: string) => {
+    deleteMutation.mutate(id);
+  };
 
   return (
     <>
@@ -34,7 +49,7 @@ export default function TodoList() {
                 <input type="checkbox" id="checkbox" name="checkbox" />
                 <label htmlFor="checkbox">{todo.title}</label>
                 <button>수정</button>
-                <button>삭제</button>
+                <button onClick={() => handleDeleteTodo(todo.id)}>삭제</button>
               </li>
             ))}
         </ul>
