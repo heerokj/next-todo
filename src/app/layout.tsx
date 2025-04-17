7a158ceeb1129f72d8db82ade6d34537555b038d
heerokj <jungheerok@gmail.com>
2025-04-17 15:08:42 +0900

    CRUD 및 완료 필터링 초기 파일 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import QueryProvider from "./providers/QueryProvider";
+import QueryProvider from "@/providers/QueryProvider";
 import type { Metadata } from "next";
 import "./globals.css";
 
---
+++ src/components/HeaderTap.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HeaderTap() {
+  return <div>완료탭</div>;
+}
