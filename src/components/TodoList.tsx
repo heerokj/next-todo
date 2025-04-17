c994333d068f289ee050f0e9752adf987d6cd959
heerokj <jungheerok@gmail.com>
2025-04-17 15:38:53 +0900

    feat : 투두리스트 읽기 기능

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,45 @@
+"use client";
+import { Todo } from "@/services/todo";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
+export const getTodos = async () => {
+  try {
+    const response = await fetch("http://localhost:3001/todos");
+    if (!response.ok) throw Error("에러 발생");
+    const todoData = await response.json();
+
+    return todoData;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export default function TodoList() {
-  return <div>TodoList</div>;
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
+
+  if (isLoading) return <div>로딩중입니다..</div>;
+  if (isError) return <div>에러 발생..</div>;
+
+  return (
+    <>
+      <section>
+        <ul>
+          {data &&
+            data.map((todo: Todo) => (
+              <li key={todo.id}>
+                <input type="checkbox" id="checkbox" name="checkbox" />
+                <label htmlFor="checkbox">{todo.title}</label>
+                <button>수정</button>
+                <button>삭제</button>
+              </li>
+            ))}
+        </ul>
+      </section>
+      <form action=""></form>
+    </>
+  );
 }
