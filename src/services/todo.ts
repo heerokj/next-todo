66345fd1677ddc63ab80f8916d0b907b1b3519d8
heerokj <jungheerok@gmail.com>
2025-04-18 18:17:18 +0900

    add : axios 에러핸들링 추가

--- src/services/todo.ts
+++ src/services/todo.ts
@@ -2,24 +2,44 @@ import { Todo } from "@/types/todo";
 import axios from "axios";
 
 export const getTodos = async () => {
-  const result = await axios.get("http://localhost:3001/todos");
-  return result.data;
+  try {
+    const result = await axios.get("http://localhost:3001/todos");
+    return result.data;
+  } catch (error) {
+    console.error("투두리스트 불러오기 실패:", error);
+    throw new Error("투두리스트 불러오기 실패");
+  }
 };
 
 export const addTodo = async (todo: Todo) => {
-  const result = await axios.post("http://localhost:3001/todos", todo);
-  return result;
+  try {
+    const result = await axios.post("http://localhost:3001/todos", todo);
+    return result;
+  } catch (error) {
+    console.error("투두리스트 추가 실패:", error);
+    throw new Error("투두리스트 추가 실패");
+  }
 };
 
 export const deleteTodo = async (id: string) => {
-  const result = await axios.delete(`http://localhost:3001/todos/${id}`);
-  return result;
+  try {
+    const result = await axios.delete(`http://localhost:3001/todos/${id}`);
+    return result;
+  } catch (error) {
+    console.error("투두리스트 삭제 실패:", error);
+    throw new Error("투두리스트 삭제 실패");
+  }
 };
 
 export const updateTodo = async (todo: Todo) => {
-  const result = await axios.put(
-    `http://localhost:3001/todos/${todo.id}`,
-    todo
-  );
-  return result;
+  try {
+    const result = await axios.put(
+      `http://localhost:3001/todos/${todo.id}`,
+      todo
+    );
+    return result;
+  } catch (error) {
+    console.error("투두리스트 수정 실패:", error);
+    throw new Error("투두리스트 수정 실패");
+  }
 };
